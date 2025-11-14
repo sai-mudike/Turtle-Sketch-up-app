d65b1c34c67bf9d190a0e403b88d3083fd5e9c89
sai <sainitinyadav.mudike@gmail.com>
2025-11-14 15:54:51 -0500

    add farward and backward movements

--- main.py
+++ main.py
@@ -3,4 +3,15 @@ from turtle import Turtle,Screen
 t1=Turtle()
 screen=Screen()
 
+def move_farwards():
+    t1.forward(10)
+
+def move_backwards():
+    t1.backward(10)
+
+
+screen.listen()
+screen.onkey(move_farwards,key='w')
+screen.onkey(move_backwards,key='s')
+
 screen.exitonclick()
